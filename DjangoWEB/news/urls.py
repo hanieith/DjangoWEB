bbfebd2ad9b767b81d7b5bc94edfc44f24e7f4c2
Constantin Shkutkov <shkutkoff@gmail.com>
2022-03-10 19:53:25 +0300

    10.03

--- DjangoWEB/news/urls.py
+++ DjangoWEB/news/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     # path('', index, name='home')
     path('', HomeNews.as_view(), name='home'),
-    path('category/<int:category_id>/', get_category, name='category'),
+    # path('category/<int:category_id>/', get_category, name='category')
+    path('category/<int:category_id>/', NewsByCategory.as_view(extra_context={'title':'Какой то титл'}), name='category'),
     path('news/<int:news_id>/', view_news, name='view_news'),
     path('news/add-news', add_news, name='add_news')
 ]
